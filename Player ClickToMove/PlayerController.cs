c4f24e6f7c350ea25facce34835c53f0ea80e6cc
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2020-04-21 00:42:46 -0500

    Continue tutorial

--- Player ClickToMove/PlayerController.cs
+++ Player ClickToMove/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour
 {
+    public Interactable focus;
+
     public LayerMask movementMask;
     Camera cam;
     PlayerMotor motor;
@@ -28,7 +30,7 @@ public class PlayerController : MonoBehaviour
                 motor.MoveToPoint(hit.point);
                 // Move our player to what we hit
 
-                // stop focusing any objects
+                RemoveFocus();
             }
         }
 
@@ -39,8 +41,40 @@ public class PlayerController : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit, 100, movementMask))
             {
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
                 /*check if we hit an interactable*/
             }
         }
+    }
+
+    void SetFocus (Interactable newFocus)
+    {
+        if (newFocus != focus)
+        {
+            if (focus != null)
+            {
+                focus.OnDefocused();
+            }
+            
+            focus = newFocus;
+            motor.FollowTarget(newFocus);
+        }
+
+        newFocus.OnFocused(transform);
+    }
+
+    void RemoveFocus()
+    {
+        if (focus != null)
+        {
+            focus.OnDefocused();
+        }
+        
+        focus = null;
+        motor.StopFollowingTarget();
     }
 }
