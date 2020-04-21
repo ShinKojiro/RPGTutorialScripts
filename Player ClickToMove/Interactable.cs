055f835aa6eaa457024762f7bf850c121e3ca423
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2020-04-21 01:17:00 -0500

    Player animation pt 1

--- Player ClickToMove/Interactable.cs
+++ Player ClickToMove/Interactable.cs
@@ -3,20 +3,28 @@
 public class Interactable : MonoBehaviour
 {
     public float radius = 3f;
+    public Transform interactionTransform;
 
     bool isFocus = false;
     Transform player;
 
     bool hasInteracted = false;
 
+    public virtual void Interact()
+    {
+        // This method is meant to be overwritten
+        Debug.Log("Interacting with" + transform.name);
+    }    
+
     private void Update()
     {
-        if (isFocus)
+        if (isFocus && !hasInteracted)
         {
-            float distance = Vector3.Distance(player.position, transform.position);
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
             if (distance <= radius)
             {
-                Debug.Log("Interact");
+                Interact();
+                hasInteracted = true;
             }
         }
     }
@@ -25,17 +33,19 @@ public class Interactable : MonoBehaviour
     {
         isFocus = true;
         player = playerTransform;
+        hasInteracted = false;
     }
 
     public void OnDefocused()
     {
         isFocus = false;
         player = null;
+        hasInteracted = false;
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, radius);
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
     }
 }
