17cf8e04c9b9b01b17738977c91cf3fece92b063
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2020-04-23 01:47:14 -0500

    Continue tutorial

---
+++ Player ClickToMove/ItemPickup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ItemPickup : Interactable
+{
+
+    public Item item;
+    public override void Interact()
+    {
+        base.Interact();
+
+        PickUp();
+    }
+
+    void PickUp()
+    {
+
+        Debug.Log("Picked up " + item.name);
+        bool wasPickUp = Inventory.instance.Add(item);
+        if (wasPickUp == true)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
